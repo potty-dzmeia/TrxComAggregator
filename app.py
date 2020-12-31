400753755a1f14fef3721eec85e4f04e1f937b91
lz1abc <ch.levkov@gmail.com>
2020-12-31 15:19:31 +0100

    initial commit - working prototype

---
+++ app.py
@@ -0,0 +1,243 @@
+import serial
+import json
+import logging.config
+import threading
+import queue
+from time import sleep
+
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
+
+
+class Settings(object):
+
+    def __init__(self, settings=None):
+
+        if settings:
+            file = settings
+        else:
+            file = "settings.cfg"
+
+        with open(file) as json_file:
+            self.app_settings = json.load(json_file)
+
+
+    def get_vports_count(self):
+        return self.app_settings["vPortNames"].__len__()
+
+
+    def get_vports(self):
+        return self.app_settings["vPortNames"]
+
+
+    def get_trx_model(self):
+        return self.app_settings["TRX"]["Model"]
+
+
+    def get_trx_port(self):
+        return self.app_settings["TRX"]["PortName"]
+
+
+    def get_trx_port_settings(self):
+        return self.app_settings["TRX"]["PortSettings"]
+
+
+class PortRedirector(object):
+
+    def __init__(self, settings):
+        self.settings = settings
+        self.transactions_queue = queue.Queue()
+        self.alive = False
+        self.log = logging.getLogger('PortRedirector')
+        self.log.setLevel(level=logging.DEBUG)
+        self.threads_vport_reader = []
+        self.thread_trxport_reader = None
+        self.thread_queue_reader = None
+
+
+    def start(self):
+        self.alive = True
+        vports = []
+
+        # Start vport_reader threads
+        for port_name in settings.get_vports():
+            ser = serial.Serial(port=port_name)
+            ser.timeout = 1  # big timeout is used to allow the threads to close if needed
+            t = threading.Thread(target=self.vport_reader, args=(ser,))
+            t.daemon = True
+            t.name = 'vport->queue'
+            t.start()
+            self.threads_vport_reader.append(t)
+            vports.append(ser)
+
+        # Start queue_reader threads
+        trx_ser = serial.Serial(port=settings.get_trx_port())
+        trx_ser.timeout = 1  # big timeout is used to allow the threads to close if needed
+        trx_ser.apply_settings(settings.get_trx_port_settings())
+        self.thread_queue_reader = threading.Thread(target=self.queue_reader, args=(trx_ser,))
+        self.thread_queue_reader.daemon = True
+        self.thread_queue_reader.name = 'queue->trx_port'
+        self.thread_queue_reader.start()
+
+        # Start queue_reader threads
+        self.thread_trxport_reader = threading.Thread(target=self.trxport_reader, args=(trx_ser, vports,))
+        self.thread_trxport_reader.daemon = True
+        self.thread_trxport_reader.name = 'trx_port->vports'
+        self.thread_trxport_reader.start()
+
+
+    def stop(self):
+        """Stop redirecting"""
+        self.log.debug('stopping')
+
+        if self.alive:
+            self.alive = False
+            for t in self.threads_vport_reader:
+                t.join()
+            self.threads_vport_reader.clear()
+            self.thread_queue_reader.join()
+            self.thread_trxport_reader.join()
+
+
+    def vport_reader(self, vport_instance):
+        """
+
+        :param vport_instance:
+        :type vport_instance: serial.Serial
+        :return:
+        """
+
+        self.log.debug('Thread START: {}'.format(vport_instance.name))
+
+        buffer = bytearray()
+
+        while self.alive:
+            try:
+                if vport_instance.in_waiting > 0:
+                    data = vport_instance.read(vport_instance.in_waiting)
+                else:
+                    data = vport_instance.read(1)
+
+                if len(data):
+                    self.log.debug("Bytes read ({}): {}".format(len(data), data))
+                    buffer += bytearray(data)
+                    trans = self.__extract_transactions(buffer, self.settings.get_trx_model())
+                    if len(trans):
+                        self.transactions_queue.put(trans)
+
+
+            except Exception as msg:
+                logger.error('{}'.format(msg))
+                break
+
+        self.alive = False
+        vport_instance.close()
+        self.log.debug('Thread END: {}'.format(vport_instance.name))
+
+
+    def queue_reader(self, trx_port_instance):
+        """
+
+        :param trx_port_instance:
+        :type trx_port_instance: serial.Serial
+        :return:
+        """
+        self.log.debug('Thread START')
+
+        while self.alive:
+            try:
+                trans = self.transactions_queue.get(timeout=1)  # Use timeout so that we can check the self.alive flag
+                self.log.debug("Transaction ---> TRX_port ({}): {}".format(trx_port_instance.name, trans))
+                trx_port_instance.write(trans)
+                trx_port_instance.flush()
+
+            except queue.Empty as msg:
+                continue
+            except Exception as msg:
+                logger.error('{}'.format(msg))
+                break
+
+        self.alive = False
+        trx_port_instance.close()
+        self.log.debug('Thread END')
+
+
+    def trxport_reader(self, trx_port_instance, vport_instances):
+        self.log.debug('Thread START')
+
+        while self.alive:
+            try:
+                if trx_port_instance.in_waiting:
+                    data = trx_port_instance.read(trx_port_instance.in_waiting)
+                else:
+                    data = trx_port_instance.read(1)
+
+                if len(data):
+                    self.log.debug("Bytes coming from TRX ({}): {}".format(len(data), data))
+                    for ser in vport_instances:
+                        ser.write(data)
+
+            except Exception as msg:
+                logger.error('{}'.format(msg))
+                break
+
+        self.alive = False
+        trx_port_instance.close()
+        self.log.debug('Thread END')
+
+
+    @classmethod
+    def __extract_transactions(cls, buffer, trx):
+        """
+        Extracts and returns all bytes which form a complete transaction from the "buffer" bytearray. The "buffer" will
+        decrease with the extracted amount of bytes.
+
+        :param buffer: array of byte from which we have to extract all valid transactions.
+        :type buffer: bytearray
+        :param trx: Brand of transceiver
+        :type trx: str
+        :return: All data up to the last terminating byte (including)
+        :rtype: bytearray
+        """
+        index = -1;
+        for i, v in enumerate(reversed(buffer)):
+            if v == cls.terminating_byte[trx]:
+                index = len(buffer) - i - 1  # index in the original list
+                break;
+
+        # No complete transaction was found
+        if index == -1:
+            return bytearray();
+
+        trans = buffer[:index + 1]
+        del buffer[:index + 1]
+        return trans
+
+
+
+    # Transaction terminating bytes for different transceivers
+    terminating_byte = {"Kenwood": 0x3B,
+                        "Yaesu": 0x3B,
+                        "Elecraft": 0x3B,
+                        "Icom": 0xFD}
+
+
+if __name__ == '__main__':
+
+    # Load settings
+    settings = Settings()
+
+    print("Trx-Com-Splitter by LZ1ABC.")
+    print("TRX ComPort is: {}".format(settings.get_trx_port()))
+    print("Virtual CommPorts are: {}".format(settings.get_vports()))
+    redirector = PortRedirector(settings)
+    redirector.start()
+
+    try:
+        while True:
+            sleep(100)
+    except KeyboardInterrupt:
+        redirector.stop()
+
+    print("73!")
---
+++ tests.py
@@ -0,0 +1,112 @@
+import unittest
+from app import PortRedirector
+from app import Settings
+
+# input  return   leftover
+kenwood_transactions = [
+    (bytearray(b"data;"), bytearray(b"data;"), bytearray(b"")),
+    (bytearray(b"data;asdf;data;asdf;333"), bytearray(b"data;asdf;data;asdf;"), bytearray(b"333")),
+    (bytearray(b"data;sdf"), bytearray(b"data;"), bytearray(b"sdf")),
+    (bytearray(b""), bytearray(b""), bytearray(b"")),
+    (bytearray(b"d"), bytearray(b""), bytearray(b"d")),
+    (bytearray(b"\x34;"), bytearray(b"4;"), bytearray(b"")),
+    (bytearray(b"\xFD;"), bytearray(b"\xFD;"), bytearray(b"")),
+    (bytearray(b";"), bytearray(b";"), bytearray(b"")),
+    (bytearray(b";;;a"), bytearray(b";;;"), bytearray(b"a")),
+    (bytearray(b"FA01234567890;"), bytearray(b"FA01234567890;"), bytearray(b"")),
+    (bytearray(b"FA01234567890;"), bytearray(b"FA01234567890;"), bytearray(b"")),
+    (bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"")),
+    (bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;IF00007267891+yyyyrx*00t11spbd1*"),
+     bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"IF00007267891+yyyyrx*00t11spbd1*"))
+]
+
+yaesu_transactions = [
+    (bytearray(b"data;"), bytearray(b"data;"), bytearray(b"")),
+    (bytearray(b"data;asdf;data;asdf;333"), bytearray(b"data;asdf;data;asdf;"), bytearray(b"333")),
+    (bytearray(b"data;sdf"), bytearray(b"data;"), bytearray(b"sdf")),
+    (bytearray(b""), bytearray(b""), bytearray(b"")),
+    (bytearray(b"d"), bytearray(b""), bytearray(b"d")),
+    (bytearray(b";"), bytearray(b";"), bytearray(b"")),
+    (bytearray(b";;;a"), bytearray(b";;;"), bytearray(b"a")),
+    (bytearray(b"FA01234567890;"), bytearray(b"FA01234567890;"), bytearray(b"")),
+    (bytearray(b"FA01234567890;"), bytearray(b"FA01234567890;"), bytearray(b"")),
+    (bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"")),
+    (bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;IF00007267891+yyyyrx*00t11spbd1*"),
+     bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"IF00007267891+yyyyrx*00t11spbd1*"))
+]
+
+elecraft_transactions = [
+    (bytearray(b"data;"), bytearray(b"data;"), bytearray(b"")),
+    (bytearray(b"data;asdf;data;asdf;333"), bytearray(b"data;asdf;data;asdf;"), bytearray(b"333")),
+    (bytearray(b"data;sdf"), bytearray(b"data;"), bytearray(b"sdf")),
+    (bytearray(b""), bytearray(b""), bytearray(b"")),
+    (bytearray(b"d"), bytearray(b""), bytearray(b"d")),
+    (bytearray(b";"), bytearray(b";"), bytearray(b"")),
+    (bytearray(b";;;a"), bytearray(b";;;"), bytearray(b"a")),
+    (bytearray(b"FA01234567890;"), bytearray(b"FA01234567890;"), bytearray(b"")),
+    (bytearray(b"FA01234567890;"), bytearray(b"FA01234567890;"), bytearray(b"")),
+    (bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"")),
+    (bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;IF00007267891+yyyyrx*00t11spbd1*"),
+     bytearray(b"IF00007267891+yyyyrx*00t11spbd1*;"), bytearray(b"IF00007267891+yyyyrx*00t11spbd1*"))
+]
+
+# input  return   leftover
+icom_transactions = [
+    [bytearray.fromhex("FE FE 5e 5c FD"), bytearray.fromhex("FE FE 5e 5c FD"), bytearray.fromhex("")],
+    [bytearray.fromhex("FE FE 5e 5c FD FE FE 00 33 FD"), bytearray.fromhex("FE FE 5e 5c FD FE FE 00 33 FD"),
+     bytearray.fromhex("")],
+    [bytearray.fromhex("FE FE 5e 5c FD FE FE 5e 5c "), bytearray.fromhex("FE FE 5e 5c FD"),
+     bytearray.fromhex("FE FE 5e 5c")],
+    [bytearray.fromhex(""), bytearray.fromhex(""), bytearray.fromhex("")],
+    [bytearray.fromhex("FD"), bytearray.fromhex("FD"), bytearray.fromhex("")],
+    [bytearray.fromhex("FD"), bytearray.fromhex("FD"), bytearray.fromhex("")],
+    [bytearray.fromhex("FD FD FD FD"), bytearray.fromhex("FD FD FD FD"), bytearray.fromhex("")],
+    [bytearray.fromhex("FE FE 5e 03 FD"), bytearray.fromhex("FE FE 5e 03 FD"), bytearray.fromhex("")],
+]
+
+
+class PortRedirector_Test(unittest.TestCase):
+
+    def test_kenwood(self):
+        for test_item in kenwood_transactions:
+            result = PortRedirector._PortRedirector__extract_transactions(test_item[0], "Kenwood")
+            self.assertTrue(result == test_item[1])
+            self.assertTrue(test_item[0] == test_item[2])
+
+    def test_yaesu(self):
+        for test_item in yaesu_transactions:
+            result = PortRedirector._PortRedirector__extract_transactions(test_item[0], "Yaesu")
+            self.assertTrue(result == test_item[1])
+            self.assertTrue(test_item[0] == test_item[2])
+
+    def test_elecraft(self):
+        for test_item in elecraft_transactions:
+            result = PortRedirector._PortRedirector__extract_transactions(test_item[0], "Elecraft")
+            self.assertTrue(result == test_item[1])
+            self.assertTrue(test_item[0] == test_item[2])
+
+    def test_icom(self):
+        for test_item in icom_transactions:
+            result = PortRedirector._PortRedirector__extract_transactions(test_item[0], "Icom")
+            self.assertTrue(result == test_item[1])
+            self.assertTrue(test_item[0] == test_item[2])
+
+
+class Settings_Test(unittest.TestCase):
+
+    def test1(self):
+        # Create object  with default settings
+        settings = Settings("test-settings.cfg")
+
+        self.assertEqual(settings.get_trx_model(), "Kenwood")
+        self.assertListEqual(settings.get_vports(), ["COM15", "COM16", "COM17"])
+        self.assertTrue(settings.get_vports_count() == 3)
+        self.assertEqual(settings.get_trx_port(), "COM9")
+        self.assertTrue(settings.get_trx_port_settings()["baudrate"] == 115200)
+        self.assertTrue(settings.get_trx_port_settings()["bytesize"] == 8)
+        self.assertTrue(settings.get_trx_port_settings()["parity"] == "N")
+        self.assertTrue(settings.get_trx_port_settings()["stopbits"] == 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
